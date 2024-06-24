1a44c00a4ac9d31609ec5f968c70be95e78d9d39
Aditya <holaaditya123@gmail.com>
2024-06-24 22:30:06 +0530

    Adding the grid section to the app..

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,35 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10">
       <div className="max-w-7xl w-full">
+        <FloatingNav
+          navItems={[
+            { name: "Home", link: "/", icon: <FaHome /> },
+            {
+              name: "About",
+              link: "#about",
+            },
+            {
+              name: "Projects",
+              link: "#projects",
+            },
+            {
+              name: "Testimonials",
+              link: "#testimonials",
+            },
+            {
+              name: "Contact",
+              link: "#contact",
+            },
+          ]}
+        />
         <Hero />
+        <Grid />
       </div>
     </main>
   );
