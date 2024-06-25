2ed3be6d3f5bea8c1863382357ebb7ef692a8bfb
Aditya <holaaditya123@gmail.com>
2024-06-25 23:03:26 +0530

    uploading...

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,14 +1,15 @@
+"use client";
+
 import { useState } from "react";
 import { IoCopyOutline } from "react-icons/io5";
 
-// Also install this npm i --save-dev @types/react-lottie
-// import Lottie from "react-lottie";
+import Lottie from "react-lottie";
 
-// import { BackgroundGradientAnimation } from "./GradientBg";
-// import GridGlobe from "./GridGlobe";
-// import animationData from "@/data/confetti.json";
+import { BackgroundGradientAnimation } from "./GradientBg";
+import animationData from "@/data/confetti.json";
 import MagicButton from "./MagicButton";
 import { cn } from "@/utils/cn";
+import GridGlobe from "./GridGlobe";
 
 export const BentoGrid = ({
   className,
@@ -50,7 +51,7 @@ export const BentoGridItem = ({
   spareImg?: string;
 }) => {
   const leftLists = ["ReactJS", "Express", "Typescript"];
-  const rightLists = ["VueJS", "NuxtJS", "GraphQL"];
+  const rightLists = ["VueJS", "NextJS", "GraphQL"];
 
   const [copied, setCopied] = useState(false);
 
@@ -110,7 +111,6 @@ export const BentoGridItem = ({
           )}
         </div>
         {id === 6 && (
-          // add background animation , remove the p tag
           <BackgroundGradientAnimation>
             <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl"></div>
           </BackgroundGradientAnimation>
