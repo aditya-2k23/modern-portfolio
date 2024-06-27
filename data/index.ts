3e3ce40eaea6c329bcf9dcdfa1154dffa531f49e
holaaditya123 <holaaditya123@gmail.com>
2024-06-27 08:17:31 +0000

    Updating the data according to me

--- data/index.ts
+++ data/index.ts
@@ -178,29 +178,29 @@ export const languages = [
 export const workExperience = [
   {
     id: 1,
-    title: "Frontend Engineer Intern",
-    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    title: "Coding",
+    desc: "I'm passionate about coding because it lets me create and solve problems in innovative ways.",
     className: "md:col-span-2",
     thumbnail: "/exp1.svg",
   },
   {
     id: 2,
-    title: "Mobile App Dev - JSM Tech",
-    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
-    className: "md:col-span-2", // change to md:col-span-2
-    thumbnail: "/exp2.svg",
+    title: "Movies",
+    desc: "I'm captivated by movies because they transport me to different realities and tell powerful stories.",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.png",
   },
   {
     id: 3,
-    title: "Freelance App Dev Project",
-    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
-    className: "md:col-span-2", // change to md:col-span-2
-    thumbnail: "/exp3.svg",
+    title: "Music",
+    desc: "I'm deeply into music because it moves my soul and lets me express my emotions creatively.",
+    className: "md:col-span-2",
+    thumbnail: "/exp3.png",
   },
   {
     id: 4,
-    title: "Lead Frontend Developer",
-    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    title: "Gaming",
+    desc: "I love gaming because it immerses me in exciting worlds and challenges my skills.",
     className: "md:col-span-2",
     thumbnail: "/exp4.svg",
   },
