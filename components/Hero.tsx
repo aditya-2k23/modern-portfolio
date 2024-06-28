a8aaa1e07629e381a94224db47c2929c9ee62453
Aditya <holaaditya123@gmail.com>
2024-06-28 23:32:48 +0530

    chore: Update Hero component with revised text and width adjustments

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -25,7 +25,7 @@ const Hero = () => {
       </div>
 
       <div className="flex justify-center relative my-20 z-10">
-        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[65vw] flex flex-col items-center justify-center">
           <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
             Dynamic Web Magic With NEXT.JS
           </h2>
@@ -36,7 +36,8 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi, I&apos;m Aditya, Student of B-Tech. based in India.
+            Hi, I&apos;m Aditya, Student of B-Tech. Computer Science Engineering
+            based in India.
           </p>
 
           <a href="#about">
